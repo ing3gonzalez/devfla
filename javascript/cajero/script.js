2c9aaaadf33de1c3054e8a4e2d59498b4b60c409
ing3gonzalez <jefry.gonzalez@gmail.com>
2023-08-17 20:41:35 -0500

    cajerov1

--- javascript/cajero/script.js
+++ javascript/cajero/script.js
@@ -1,36 +1,51 @@
+const accountSelect = document.getElementById("accountSelect");
+const consultarButton = document.getElementById("consultar");
+const retirarButton = document.getElementById("retirar");
+const consignarButton = document.getElementById("consignar");
+const outputDiv = document.getElementById("output");
+
+const accounts = {
+  Mali: 1000,
+  Gera: 1500,
+  Maui: 2000,
+};
+
+consultarButton.addEventListener("click", () => {
+  const selectedAccount = accountSelect.value;
+  const balance = accounts[selectedAccount];
+  outputDiv.innerText = `Saldo de ${selectedAccount}: $${balance}`;
+});
+
+consignarButton.addEventListener("click", () => {
+  const selectedAccount = accountSelect.value;
+  const balance = accounts[selectedAccount];
+  const valorconsignado = parseFloat(prompt("Ingrese la cantidad a consignar:"));
+
+  if (isNaN(valorconsignado) || valorconsignado <= 0) {
+    alert("Ingrese una cantidad válida para consignar.");
+    return;
+  }
+
+  accounts[selectedAccount] += valorconsignado;
+  outputDiv.innerText = `Consignaste $${valorconsignado}. Saldo nuevo: $${accounts[selectedAccount]}`;
+});
 
 
-const input = document.querySelector('#consigna')
-const buttonDeposito = document.querySelector('#boton_deposita')
-const dineroencuenta = document.querySelector('#boton_consulta')
-const cuentaPersonal =
-{
-    saldo:0,
-    nombre: "Jefry",
-    cuenta:"1010",
-    password:"1010"  
-}
+retirarButton.addEventListener("click", () => {
+  const selectedAccount = accountSelect.value;
+  const balance = accounts[selectedAccount];
+  const withdrawalAmount = parseFloat(prompt("Ingrese la cantidad a retirar:"));
 
-buttonDeposito.addEventListener('click',function(e)
-{
-    console.log(e)
+  if (isNaN(withdrawalAmount) || withdrawalAmount <= 0) {
+    alert("Ingrese una cantidad válida para retirar.");
+    return;
+  }
 
+  if (withdrawalAmount > balance) {
+    alert("Fondos insuficientes.");
+    return;
+  }
 
-    const deposito = input.value
-    const numerdeposito = parseInt(deposito)
-    console.log (typeof deposito)
-
-    const saldo =  cuentaPersonal.saldo 
-
-    const resultado =  saldo  + deposito
-
-    saldo.innerText=resultado
-
-}
-)
-var cuentas = [
-  { nombre: 'Mali', saldo: 200 },
-  { nombre: 'Gera', saldo: 290 },
-  { nombre: 'Maui', saldo: 67 }
-];
-
+  accounts[selectedAccount] -= withdrawalAmount;
+  outputDiv.innerText = `Retiraste $${withdrawalAmount}. Saldo restante: $${accounts[selectedAccount]}`;
+});
