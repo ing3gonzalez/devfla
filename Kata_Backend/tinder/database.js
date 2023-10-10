654a12a06147aaef78941134938249e67cce1cdc
ing3gonzalez <jefry.gonzalez@gmail.com>
2023-10-10 15:44:42 -0500

    Update database.js

--- Kata_Backend/tinder/database.js
+++ Kata_Backend/tinder/database.js
@@ -12,7 +12,7 @@ const pgp = require('pg-promise')();
 const dbConfig = {
   user: 'tinder_39hv_user',
   password: 'zdsHf6H69DK88K5L8tZ4kw3esnLkUde9',
-  host: 'dpg-ckirbjb6fquc739mhsk0-a',
+  host: 'postgres://tinder_39hv_user:zdsHf6H69DK88K5L8tZ4kw3esnLkUde9@dpg-ckirbjb6fquc739mhsk0-a.oregon-postgres.render.com/tinder_39hv',
   port: '5432',
   database: 'tinder_39hv',
 };
